23a3323309555798c050a7b2fee68175de95b3f1
Andrew Shepard <shep.andy@gmail.com>
2020-08-29 05:59:51 -0400

    Reduce Rx chain

--- Shared/ServiceTypesProvider.swift
+++ Shared/ServiceTypesProvider.swift
@@ -10,7 +10,6 @@ import Foundation
 import Combine
 
 final class ServiceTypesProvider {
-    
     private let browser: NetServiceBrowser
     
     init(browser: NetServiceBrowser = NetServiceBrowser()) {
@@ -24,26 +23,22 @@ final class ServiceTypesProvider {
 
 extension ServiceTypesProvider {
     func publisherForServiceTypes() -> AnyPublisher<[NetService], Error> {
-        var results: [NetService] = []
-        
         return browser.publisherForSearchTypes()
-            .map { services -> [NetService] in
-                results.append(contentsOf: services)
-                let sorted = results.unique.sorted(by: { $0.name < $1.name })
-                return sorted
-            }
+            .scan([NetService](), { (existing, updated) -> [NetService] in
+                var services = existing
+                services.append(contentsOf: updated)
+                return services.unique.sorted(by: { $0.name < $1.name })
+            })
             .eraseToAnyPublisher()
     }
     
     func publisherForServices(for serviceType: NetService) -> AnyPublisher<[NetService], Error> {
-        var results: [NetService] = []
-        
         return browser.publisherForServices(type: serviceType.query)
-            .map { services -> [NetService] in
-                results.append(contentsOf: services)
-                let sorted = results.unique.sorted(by: { return $0.name > $1.name })
-                return sorted
-            }
+            .scan([NetService](), { (existing, updated) -> [NetService] in
+                var services = existing
+                services.append(contentsOf: updated)
+                return services.unique.sorted(by: { $0.name < $1.name })
+            })
             .eraseToAnyPublisher()
     }
     
@@ -57,35 +52,24 @@ extension ServiceTypesProvider {
                 var result: [String: String] = [:]
                 result["IPv4"] = service.addressIPv4
                 result["IPv6"] = service.addressIPv6
-
                 return result
             }
             .share()
             .eraseToAnyPublisher()
         
         return Publishers.CombineLatest(servicePublisher, addressesPublisher)
-            .map { (resolved, addresses) -> ((NetService, [String: Data]), [String: String]) in
-                guard let data = service.txtRecordData() else { return ((resolved, [:]), addresses) }
-                return ((service, NetService.dictionary(fromTXTRecord: data)), addresses)
-            }
-            .map { (result) -> [String: String] in
-                let ((_, dataDictionary), addresses) = result
-                var dictionary = dataDictionary.mapValues { (value) -> String in
-                    return String(data: value, encoding: .utf8) ?? ""
-                }
-
-                addresses.forEach { (key, value) in
-                    dictionary[key] = value
-                }
-
-                return dictionary
-            }
-            .map { (dictionary) -> [Entry] in
-                dictionary.reduce(into: [Entry]()) { (accumlator, input) in
-                    let (key, value) = input
-                    let entry = Entry(title: key, subtitle: value)
-                    accumlator.append(entry)
-                }
+            .map { (resolved, addresses) -> [Entry] in
+                guard let data = service.txtRecordData() else { return [] }
+                
+                return NetService.dictionary(fromTXTRecord: data)
+                    .mapValues { String(data: $0, encoding: .utf8) ?? "" }
+                    .reduce(into: addresses) { (dictionary, keyvalue) in
+                        dictionary[keyvalue.0] = keyvalue.1
+                    }
+                    .reduce(into: [Entry]()) { (accumulator, keyvalue) in
+                        let entry = Entry(title: keyvalue.0, subtitle: keyvalue.1)
+                        accumulator.append(entry)
+                    }
             }
             .eraseToAnyPublisher()
     }
